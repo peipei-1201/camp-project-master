7473c925a251e8e9c910fa61d73dc3663f4d910b
李柔霈 <pei931201@gmail.com>
2022-07-03 14:57:53 +0800

    1

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -35,7 +35,7 @@ const makeClass = (i) => (
 						v-if="activity.url"
 						:href="activity.url"
 						target="_blank"
-						class="focus:outline-none rounded py-3 px-4 text-white font-extrabold border border-white flex justify-center items-center text-base hover:bg-white bg-transparent">
+						class="focus:outline-none rounded py-3 px-4 text-white font-extrabold border border-white flex justify-center items-center text-base hover:bg-gray-400 bg-transparent">
             Follow Me On Instagram
 					</a>
 
